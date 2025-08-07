0d6d092650adda5d195ff005e5f7eaebd408da6b
Rohit-KumarRajak <rohitkrrajak993455@gmail.com>
2025-08-07 14:50:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -875,7 +875,7 @@ function goBack() {
 
   // 🟡 Reset Note to default
   const noteContent = document.getElementById('note-content');
-  noteContent.innerText = "More stalls and food options will be added. Stay tuned!br><br>
+  noteContent.innerText = "More stalls and food options will be added. Stay tuned!
       💬 After placing your order on WhatsApp, if there's a delay in response — whether the message is seen or not — you can make a WhatsApp call to the stall directly using the displayed number.";
 
   // 🟡 Reset Menu Heading
